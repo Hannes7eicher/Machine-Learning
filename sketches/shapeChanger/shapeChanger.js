e753736983803118fc9505dfaf6d24ec222fc8ec
HanZS799 <hannes7eicher@gmail.com>
2020-10-21 12:42:21 +0200

    pose

--- sketches/shapeChanger/shapeChanger.js
+++ sketches/shapeChanger/shapeChanger.js
@@ -8,7 +8,7 @@ function setup() {
     video = createCapture(VIDEO);
     video.hide();
     poseNet = ml5.poseNet(video, modelLoaded);
-    poseNet.on('poses', gotPoses);
+    poseNet.on('pose', gotPoses);
 }
 
 
