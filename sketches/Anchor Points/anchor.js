acd93ef2677ff6db2350d36912227010045cb914
dinapitkanen <58941214+dinapitkanen@users.noreply.github.com>
2020-10-28 14:51:27 +0100

    added speed variables for each ellipse

--- sketches/Anchor Points/anchor.js
+++ sketches/Anchor Points/anchor.js
@@ -17,8 +17,19 @@ let state = false;
 
 
 x = 0;
-speed = 3;
+speed1 = 3;
+speed2 = 3;
+speed3 = 3;
+speed4 = 3;
+speed5 = 3;
+speed6 = 3;
 
+speed7 = 3;
+speed8 = 3;
+speed9 = 3;
+speed10 = 3;
+speed11 = 3;
+speed12 = 3;
 // variable for ellipse size
 
 let eSize = 30;
@@ -29,6 +40,13 @@ let el3 = 0;
 let el4 = 0;
 let el5 = 0;
 let el6 = 0;
+// variables for ellipses on y axis
+let el7 = 0;
+let el8 = 0;
+let el9 = 0;
+let el10 = 0;
+let el11 = 0;
+let el12 = 0;
 
   
 
@@ -91,7 +109,7 @@ function draw() {
     
 
    /// ellipse along y axis
-
+  
    ellipse(67, el1, 40, eSize);
    ellipse(127, el2, 40, eSize);
    ellipse(187, el3, 40, eSize);
@@ -117,104 +135,165 @@ function draw() {
 
     if(pose) {
         
-        ellipse(pose.rightWrist.x, pose.rightWrist.y, 10);
-        ellipse(pose.leftWrist.x, pose.leftWrist.y, 10);
-        ellipse(pose.rightAnkle.x, pose.rightAnkle.y, 10);
-        ellipse(pose.leftAnkle.x, pose.leftAnkle.y, 10);
-        ellipse(pose.rightShoulder.x, pose.rightShoulder.y, 10);
-        ellipse(pose.leftShoulder.x, pose.leftShoulder.y, 10);
-        ellipse(pose.leftHip.x, pose.leftHip.y, 10);
-        ellipse(pose.rightHip.x, pose.rightHip.y, 10);
-        ellipse(pose.rightKnee.x, pose.rightKnee.y, 10);
-        ellipse(pose.leftKnee.x, pose.leftKnee.y, 10);
-        ellipse(pose.leftEye.x, pose.leftEye.y, 10);
-        ellipse(pose.rightEye.x, pose.rightEye.y, 10);
-        ellipse(pose.nose.x, pose.nose.y, 10);
+        // noStroke();
+        // ellipse(pose.rightWrist.x, pose.rightWrist.y, 10);
+        // ellipse(pose.leftWrist.x, pose.leftWrist.y, 10);
+        // ellipse(pose.rightAnkle.x, pose.rightAnkle.y, 10);
+        // ellipse(pose.leftAnkle.x, pose.leftAnkle.y, 10);
+        // ellipse(pose.rightShoulder.x, pose.rightShoulder.y, 10);
+        // ellipse(pose.leftShoulder.x, pose.leftShoulder.y, 10);
+        // ellipse(pose.leftHip.x, pose.leftHip.y, 10);
+        // ellipse(pose.rightHip.x, pose.rightHip.y, 10);
+        // ellipse(pose.rightKnee.x, pose.rightKnee.y, 10);
+        // ellipse(pose.leftKnee.x, pose.leftKnee.y, 10);
+        // ellipse(pose.leftEye.x, pose.leftEye.y, 10);
+        // ellipse(pose.rightEye.x, pose.rightEye.y, 10);
+        // ellipse(pose.nose.x, pose.nose.y, 10);
 
-        // declaring movement direction of according to width
-    el1 = el1 + speed;
+        
+
+        // declaring movement directions of ellipses according to width
+    el1 = el1 + speed1;
     if (el1 > width) {
-        speed *= -1;
+        speed1 *= -1;
     }
     if (el1 == 0) {
-        speed *= -1;
+        speed1 *= -1;
     }
 
-    el2 = el2 + speed;
+    el2 = el2 + speed2;
     if (el2 > width) {
-        speed *= -1;
+        speed2 *= -1;
     }
 
     if (el2 == 0) {
-        speed *= -1;
+        speed2 *= -1;
     }
     
-    el3 = el3 + speed;
+    el3 = el3 + speed3;
         if (el3 > width) {
-            speed *= -1;
+            speed3 *= -1;
         }
         if (el3 == 0) {
-            speed *= -1;
+            speed3 *= -1;
         }
 
-    el4 = el4 + speed;
+    el4 = el4 + speed4;
         if (el4 > width) {
-            speed *= -1;
+            speed4 *= -1;
         }
         if (el4 == 0) {
-            speed *= -1;
+            speed4 *= -1;
         }
 
-    el5 = el5 + speed;
+    el5 = el5 + speed5;
         if (el5 > width) {
-            speed *= -1;
+            speed4 *= -1;
         }
         if (el5 == 0) {
-            speed *= -1;
+            speed5 *= -1;
         }
 
-     el6 = el6 + speed;
+     el6 = el6 + speed6;
         if (el6 > width) {
-            speed *= -1;
+            speed6 *= -1;
         }
         if (el6 == 0) {
-            speed *= -1;
+            speed6 *= -1;
         } 
 
-        //reading position of body parts to manipulate ellipses 
-        // if (pose.leftWrist.y || pose.rightWrist.y < pose.nose.y) {
+        el7 = el7 + speed7;
+        if (el7 > height) {
+            speed7 *= -1;
+        }
+        if (el7 == 0) {
+            speed7 *= -1;
+        }
+    
+        el8 = el8 + speed8;
+        if (el8 > height) {
+            speed8 *= -1;
+        }
+    
+        if (el8 == 0) {
+            speed8 *= -1;
+        }
+        
+        el9 = el9 + speed9;
+            if (el9 > height) {
+                speed9 *= -1;
+            }
+            if (el9 == 0) {
+                speed9 *= -1;
+            }
+    
+        el10 = el10 + speed10;
+            if (el10 > height) {
+                speed10 *= -1;
+            }
+            if (el10 == 0) {
+                speed10 *= -1;
+            }
+    
+        el11 = el11 + speed11;
+            if (el11 > height) {
+                speed11 *= -1;
+            }
+            if (el11 == 0) {
+                speed11 *= -1;
+            }
+    
+         el12 = el12 + speed12;
+            if (el12 > height) {
+                speed12 *= -1;
+            }
+            if (el12 == 0) {
+                speed12 *= -1;
+            } 
 
-        //     el2 = 3;
+        //reading position of body parts to manipulate individual ellipses behavior
 
-        // }
+        if (pose.leftWrist.y || pose.rightWrist.y < pose.nose.y) {
+
+            el2 = 0;
+
+        }
 
         if (pose.leftWrist.y > pose.nose.y && pose.rightWrist.y < pose.nose.y) {
 
             el1 = 0;
         }
+
+        if(pose.leftHip.x > width/2) {
+
+            el6 = 0;
+            el5 = 0;
+        }
+
+
         
     }
 
     
       
-    }
+    };
  
 
 
 // draw rectangle and cut out circle. Supposed to be used so video caption is revealed in the cut out.
 // NOT USED ATM
-    function drawRect() {
-        //image(video, 0, 0);
+    // function drawRect() {
+    //     //image(video, 0, 0);
 
-        //draw();
+    //     //draw();
         
-        rect(0, 0, 620, 55);
-        fill('pink');
-        noStroke();
+    //     rect(0, 0, 620, 55);
+    //     fill('pink');
+    //     noStroke();
 
-        rect(0, 0, 620, 55);
-        fill('blue');
-        noStroke();
+    //     rect(0, 0, 620, 55);
+    //     fill('blue');
+    //     noStroke();
 
     //erases ellipse within black rectangle
         // erase(200, 0);
@@ -222,15 +301,15 @@ function draw() {
         // noErase();
 
     
-    }
+    // }
 
     // NOT USED ATM
 
-   let reacher;
+//    let reacher;
 
-    function reach() {
-    background(220);
-    stroke(9);
-    line(width/2, 0, width/2, frameCount*0.2);
-}
+//     function reach() {
+//     background(220);
+//     stroke(9);
+//     line(width/2, 0, width/2, frameCount*0.2);
+// }
       
